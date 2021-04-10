d7d404e78970aaa0ae587a47cce872307e6451a4
liyiming-china <593346684@qq.com>
2021-04-10 12:08:19 +0800

    refactor cal

--- src/main/java/Cal.java
+++ src/main/java/Cal.java
@@ -1,13 +1,7 @@
 public class Cal {
     public int compute(int i, int i1, char c) {
-        switch (c){
-            case '+':
-                return i+i1;
-            case '-':
-                return i-i1;
-            default:
-                throw new IllegalArgumentException();
-        }
+        Computable compute = ComputerFactory.getCompute(c);
+        return compute.compute(i, i1);
 
     }
 }
